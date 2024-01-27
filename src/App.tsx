2f8a261f86274ab0451d1dda4e817ec7f6de8876
Priyankesh <priyankeshom@gmail.com>
2024-01-27 11:41:47 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello FlickPix!
+    </h1>
+  )
+}
+
+export default App
